cd8a7bf0ee63e2c5b5326c369a8be515444fcbe9
Vikas Solanki <vikassolankics@gmail.com>
2019-09-21 12:57:43 +0530

    Create Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,25 @@
+package com.designpattern;
+
+class Singleton {
+    private String name = "singleton";
+
+    static class SingletonInstance {
+        private static final Singleton instance = new Singleton();
+    }
+    private Singleton(){}
+
+    public static Singleton getInstance() {
+        return SingletonInstance.instance;
+    }
+
+    public void singletonFun() {
+        System.out.println(name);
+    }
+}
+
+public class Singleton {
+    public static void main(String args[]) {
+        Singleton singleton = Singleton.getInstance();
+        singleton.singletonFun();
+    }
+}
